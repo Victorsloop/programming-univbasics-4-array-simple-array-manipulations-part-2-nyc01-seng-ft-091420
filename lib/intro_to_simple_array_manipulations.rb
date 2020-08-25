aee15c8adbb8c8ace2e49c62ad9505c01a34f13b
Victor Torres <torres.victor136@gmail.com>
2020-08-25 21:50:45 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(arraya,arrayb)
+  arraya.concat(arrayb)
+end 
+
+def using_insert( arraya, element)
+  arraya.insert(4,element)
+end 
+
+def using_uniq(arraya)
+  arraya.uniq
+end 
+
+def using_flatten(arraya)
+  arraya.flatten 
+end 
+
+def using_delete(arraya, string)
+  arraya.delete(string)
+end 
+
+def using_delete_at(arraya, integer)
+  arraya.delete_at(integer)
+end 
