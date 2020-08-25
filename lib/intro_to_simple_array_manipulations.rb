9fcb925730760b2dfdc85f2467c9a877102e0cd1
Victor Torres <torres.victor136@gmail.com>
2020-08-25 21:47:31 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,11 @@
+def using_concat(arraya,arrayb)
+  arraya.concat(arrayb)
+end 
+
+def using_insert( arraya, element)
+  arraya.insert(4,element)
+end 
+
+def using_uniq(arraya)
+  arraya.uniq 
+end 
